4f84882e81c5e076ed4fd3d47ac9c2d25d691e65
memorizu <kaidohmary@gmail.com>
2026-07-10 10:50:28 +0300

    complete 1.base-concepts/task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,28 @@
 "use strict"
+
 function solveEquation(a, b, c) {
+
   let arr = [];
-  
+  let discriminant = b ** 2 - (4 * a * c);
+
+  if (discriminant < 0) {
+    return arr;
+  } else if (discriminant === 0) {
+    arr.push(-b / (2 * a));
+  } else {
+    arr.push((-b + Math.sqrt(discriminant)) / (2 * a));
+    arr.push((-b - Math.sqrt(discriminant)) / (2 * a));
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+  
+  let percentPerMonth = percent / 12 / 100;
+  let creditBody = amount - contribution;
+  let paymentPerMonth = creditBody * (percentPerMonth + (percentPerMonth / (((1 + percentPerMonth) ** countMonths) - 1)));
+
+  return +(paymentPerMonth * countMonths).toFixed(2);
   
 }
