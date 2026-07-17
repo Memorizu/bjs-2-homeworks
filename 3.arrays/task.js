d1017aea6184909b997d73aa9a3c83b0880ef95e
memorizu <kaidohmary@gmail.com>
2026-07-17 16:08:24 +0300

    complete 3.arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
+
+  let isCompare = arr1.every((elem, i) => elem === arr2[i]) && arr1.length === arr2.length;
+  return isCompare
   
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+  const filteredUsers = users.filter(user => user.gender === gender);
+
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+
+  return filteredUsers.reduce((sum, user) => sum + user.age, 0) / filteredUsers.length;
+
 }
